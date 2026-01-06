deced5803bd5bbcc6f6f01f9e009d85bb3e898ed
jibinkp <jibinkp770@gmail.com>
2026-01-06 21:09:06 +0530

    Implementing Add transaction dialog UI

--- app/src/main/java/com/example/expensetracker/ui/transaction/AddTransactionDialog.kt
+++ app/src/main/java/com/example/expensetracker/ui/transaction/AddTransactionDialog.kt
@@ -3,10 +3,19 @@ package com.example.expensetracker.ui.transaction
 import android.content.Context
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatDialog
+import com.example.expensetracker.databinding.LayoutAddTransactionDialogBinding
 
-class AddTransactionDialog(context: Context): AppCompatDialog(context) {
+class AddTransactionDialog(context: Context) : AppCompatDialog(context) {
+    private lateinit var binding: LayoutAddTransactionDialogBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = LayoutAddTransactionDialogBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initView()
+    }
+
+    private fun initView() {
+
     }
 }
