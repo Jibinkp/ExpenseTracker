bf69778057f2cacb63cfbe08511d12b592558457
jibinkp <jibinkp770@gmail.com>
2026-01-07 20:23:48 +0530

    Implementing Add transaction dialog UI

--- app/src/main/java/com/example/expensetracker/ui/transaction/TransactionActivity.kt
+++ app/src/main/java/com/example/expensetracker/ui/transaction/TransactionActivity.kt
@@ -6,16 +6,27 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.expensetracker.R
+import com.example.expensetracker.databinding.ActivityTransactionBinding
 
 class TransactionActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityTransactionBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_transaction)
+        binding = ActivityTransactionBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        initView()
+    }
+
+    private fun initView() {
+        binding.fbAddTransaction.setOnClickListener {
+            AddTransactionDialog(this).show()
         }
     }
 }
