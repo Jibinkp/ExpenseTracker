195592a7594b429d5834f62279c3738f7ed7b23f
jibinkp <jibinkp770@gmail.com>
2026-03-17 22:43:33 +0530

    UI issue fixing

--- app/src/main/java/com/example/expensetracker/MainActivity.kt
+++ app/src/main/java/com/example/expensetracker/MainActivity.kt
@@ -31,7 +31,7 @@ class MainActivity : AppCompatActivity() {
 
         ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, 0)
             insets
         }
         AppCenter.start(application, ContextCompat.getString(this,R.string.app_center_secret_key), Analytics::class.java, Crashes::class.java,
